224db8bf252cd983bed020b80411538240fee8f0
Belkys02 <113115054+Belkys02@users.noreply.github.com>
2022-09-30 14:04:28 -0500

    Create Compara.cpp

---
+++ Compara.cpp
@@ -0,0 +1,26 @@
+# incluir < iostream >
+utilizando el espacio de  nombres  estándar ;
+int  principal ()
+{
+	flotar a,b;
+	cout<< " ingrese ab: " ; cin>>a>>b;
+	si (a > b)
+	{
+	  cout<<a<< " es mayor a " <<b<<endl;
+
+
+	} más {
+
+	  cout<<b<< " es mayor a " <<a<<endl;
+
+
+	}
+
+	devolver  0 ;
+
+
+
+
+
+
+}
